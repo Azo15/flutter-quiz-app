161fa7aef57aff4dc9cf5ad3440249895bae0169
Azo Ismail <ismailazo260@gmail.com>
2026-04-15 07:43:03 +0300

    feat: remove unused dependencies and update text theme font in QuizApp

--- flutter_quiz_app/lib/main.dart
+++ flutter_quiz_app/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'screens/quiz_screen.dart';
 
 void main() {
@@ -18,8 +17,10 @@ class QuizApp extends StatelessWidget {
         brightness: Brightness.dark,
         scaffoldBackgroundColor: const Color(0xFF0F172A), // Tailwind Slate 900
         primaryColor: const Color(0xFF6366F1), // Tailwind Indigo 500
-        textTheme: GoogleFonts.interTextTheme(
-          Theme.of(context).textTheme.apply(bodyColor: Colors.white, displayColor: Colors.white),
+        textTheme: Theme.of(context).textTheme.apply(
+          bodyColor: Colors.white, 
+          displayColor: Colors.white,
+          fontFamily: 'Roboto',
         ),
         useMaterial3: true,
       ),
